ff862c3dea1cc04370b21513e12cb91319511b94
Eric Berseth <eric.berseth@particle.io>
2021-06-17 22:41:45 -0700

    add peek size

--- test/main.cpp
+++ test/main.cpp
@@ -73,33 +73,35 @@ int main() {
         exit(-1);
     }
 #if 1
-    theQueue.push_back("Hello there 1", 0);
-    theQueue.push_back("Hello here 2", 12);
-    theQueue.push_back("Hello where 3", 13);
-    theQueue.push_back("Hello where 4", 13);
-    theQueue.push_back("Hello where 5", 13);
-    theQueue.push_back("Hello where 6", 13);
-    theQueue.push_back("Hello where 7", 13);
-    theQueue.push_back("Hello where 8", 13);
-    theQueue.push_back("Hello where 9", 13);
-    theQueue.push_back("Hello where 10", 14);
-    theQueue.push_back("Hello where 11", 14);
-    theQueue.push_back("Hello where 12", 14);
-    theQueue.push_back("Hello where 13", 14);
-    theQueue.push_back("Hello where 14", 14);
-    theQueue.push_back("Hello where 15", 14);
-    theQueue.push_back("Hello where 16", 14);
-    theQueue.push_back("Hello where 17", 14);
-    theQueue.push_back("Hello where 18", 14);
-    theQueue.push_back("Hello where 19", 14);
+    theQueue.push_back((uint8_t*)"Hello there 1", 0);
+    theQueue.push_back((uint8_t*)"Hello here 2", 12);
+    theQueue.push_back((uint8_t*)"Hello where 3", 13);
+    theQueue.push_back((uint8_t*)"Hello where 4", 13);
+    theQueue.push_back((uint8_t*)"Hello where 5", 13);
+    theQueue.push_back((uint8_t*)"Hello where 6", 13);
+    theQueue.push_back((uint8_t*)"Hello where 7", 13);
+    theQueue.push_back((uint8_t*)"Hello where 8", 13);
+    theQueue.push_back((uint8_t*)"Hello where 9", 13);
+    theQueue.push_back((uint8_t*)"Hello where 10", 14);
+    theQueue.push_back((uint8_t*)"Hello where 11", 14);
+    theQueue.push_back((uint8_t*)"Hello where 12", 14);
+    theQueue.push_back((uint8_t*)"Hello where 13", 14);
+    theQueue.push_back((uint8_t*)"Hello where 14", 14);
+    theQueue.push_back((uint8_t*)"Hello where 15", 14);
+    theQueue.push_back((uint8_t*)"Hello where 16", 14);
+    theQueue.push_back((uint8_t*)"Hello where 17", 14);
+    theQueue.push_back((uint8_t*)"Hello where 18", 14);
+    theQueue.push_back((uint8_t*)"Hello where 19", 14);
 #else
     char buf[1024] = {};
     size_t s = 0;
     bool peekret;
     do {
-        peekret = theQueue.peek_front(buf, s);
+        auto size = theQueue.peek_front_size();
+        peekret = theQueue.peek_front((uint8_t*)buf, s);
         if (peekret) theQueue.pop_front();
-        printf("Peeked: %d/%lu: %s\n", (int)peekret, s, buf);
+        else break;
+        printf("Peeked: %d/%lu/%lu: %s\n", (int)peekret, size, s, buf);
         memset(buf, 0, sizeof(buf));
     } while (peekret);
 #endif
